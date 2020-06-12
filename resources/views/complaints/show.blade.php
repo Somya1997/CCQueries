5593f88ea258c613897da6051f760d0031e3698c
VISHNUKANT-VISHNU <vishnutrd@gmail.com>
2020-06-12 16:51:25 +0530

    Normal changes

--- resources/views/complaints/show.blade.php
+++ resources/views/complaints/show.blade.php
@@ -1,15 +1,20 @@
 @extends('main')
-<!-- @section('heading')
+@section('heading')
 <div class="top-bar">
     <div class="container">
       <div class="row">
-          <div class="col-12">
-            <center><h2 style="font-size: 50px;color:darkorange"> CC QUERIES</h2></center>
-          </div>
+        <div class="col-6">
+            <div class="site-logo">
+                <a href="{{url('/')}}"><img src="../images/logonew1.png" alt=" " width="50%" height="2%"></a>
+            </div>
+        </div>
+        <div class="col-6 text-right ml-auto ">
+            <h2 style="font-size: 400%;color:darkorange;margin-top:3%"> CC QUERIES</h2>
         </div>
       </div>
+    </div>
 </div>
-@endsection -->
+@endsection
 @section('content')
         <div class="content">
 	        <div class="content1">
