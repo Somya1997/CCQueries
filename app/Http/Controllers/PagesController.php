3e462d2c3f887ef578017c5a90ca2738f05fb055
Somya1997 <43964018+Somya1997@users.noreply.github.com>
2020-06-30 17:43:00 +0530

    staff dashboard made dynamic

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\StudentMnnit;
+use App\ComplaintMnnit;
+use Auth;
 
 use Illuminate\Http\Request;
 
@@ -9,8 +12,21 @@ class PagesController extends Controller
     public function getIndex(){
         return view('pages.home');
     }
+    
     public function getDashboard()
     {
-        return view('pages.dashboard');
+        if(Auth::user()->name=='Admin')
+        {
+            $complaints = ComplaintMnnit::join('student_mnnits', 'id', '=', 'student_id') ->where('status','=',0)->get();
+        }
+        else
+        {
+        $complaints = ComplaintMnnit::join('student_mnnits', 'id', '=', 'student_id')
+        ->where('status','=',1)
+        ->where('staff','=',Auth::user()->name)
+        ->get();
+        }
+        return view('pages.dashboard')->withComplaints($complaints);
     }
+
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -57,150 +57,26 @@
                                             <tr>
                                                 <th>Complaint Number</th>
                                                 <th>Student Name</th>
-                                                <th>Hostel</th>
-                                                <th>Room </th>
+                                                <th>Registration No</th>
+                                                <th>Address</th>
                                                 <th>Contact</th>
                                                 <th>Availability</th>
                                                 <th>Nature Of Problem</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>Siddharth</td>
-                                                <td>PG hostel</td>
-                                                <td>44</td>
-                                                <td class="process">Processed</td>
-                                                <td>8381825891</td>
-                                                <td>wifi not working</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-28 01:22</td>
-                                                <td>Mobile</td>
-                                                <td>Samsung S8 Black</td>
-                                                <td>44</td>
-                                                <td class="process">Processed</td>
-                                                <td>$756.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-27 02:12</td>
-                                                <td>Game</td>
-                                                <td>Game Console Controller</td>
-                                                <td>44</td>
-                                                <td class="denied">Denied</td>
-                                                <td>$22.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-26 23:06</td>
-                                                <td>Mobile</td>
-                                                <td>iPhone X 256Gb Black</td>
-                                                <td>44</td>
-                                                <td class="denied">Denied</td>
-                                                <td>$1199.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-25 19:03</td>
-                                                <td>Accessories</td>
-                                                <td>USB 3.0 Cable</td>
-                                                <td>44</td>
-                                                <td class="process">Processed</td>
-                                                <td>$10.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-29 05:57</td>
-                                                <td>Accesories</td>
-                                                <td>Smartwatch 4.0 LTE Wifi</td>
-                                                <td>44</td>
-                                                <td class="denied">Denied</td>
-                                                <td>$199.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-24 19:10</td>
-                                                <td>Camera</td>
-                                                <td>Camera C430W 4k</td>
-                                                <td>44</td>
-                                                <td class="process">Processed</td>
-                                                <td>$699.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-22 00:43</td>
-                                                <td>Computer</td>
-                                                <td>Macbook Pro Retina 2017</td>
-                                                <td>44</td>
-                                                <td class="process">Processed</td>
-                                                <td>$10.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-26 23:06</td>
-                                                <td>Mobile</td>
-                                                <td>iPhone X 256Gb Black</td>
-                                                <td>44</td>
-                                                <td class="denied">Denied</td>
-                                                <td>$1199.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-26 23:06</td>
-                                                <td>Mobile</td>
-                                                <td>iPhone X 256Gb Black</td>
-                                                <td>44</td>
-                                                <td class="denied">Denied</td>
-                                                <td>$1199.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-26 23:06</td>
-                                                <td>Mobile</td>
-                                                <td>iPhone X 256Gb Black</td>
-                                                <td>44</td>
-                                                <td class="denied">Denied</td>
-                                                <td>$1199.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-26 23:06</td>
-                                                <td>Mobile</td>
-                                                <td>iPhone X 256Gb Black</td>
-                                                <td>44</td>
-                                                <td class="denied">Denied</td>
-                                                <td>$1199.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-26 23:06</td>
-                                                <td>Mobile</td>
-                                                <td>iPhone X 256Gb Black</td>
-                                                <td>44</td>
-                                                <td class="denied">Denied</td>
-                                                <td>$1199.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-26 23:06</td>
-                                                <td>Mobile</td>
-                                                <td>iPhone X 256Gb Black</td>
-                                                <td>44</td>
-                                                <td class="denied">Denied</td>
-                                                <td>$1199.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                            <tr>
-                                                <td>2018-09-26 23:06</td>
-                                                <td>Mobile</td>
-                                                <td>iPhone X 256Gb Black</td>
-                                                <td>44</td>
-                                                <td class="denied">Denied</td>
-                                                <td>$1199.00</td>
-                                                <td>lan</td>
-                                            </tr>
-                                       </tbody>
+                                            @foreach ($complaints as $complaint)
+                                                <tr>
+                                                <th>{{$complaint->student_id}}</th>
+                                                <td>{{$complaint->name}}</td>
+                                                <td>{{$complaint->regno}}</td>
+                                                <td>{{$complaint->hostel}}, Room No. {{$complaint->room}}</td>
+                                                <td>{{$complaint->phoneno}}</td>
+                                                <td>{{$complaint->availabledate}}   {{$complaint->availabletime}}</td>
+                                                <td>{{$complaint->nature}}</td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
                                     </table>
                                 </div>
                                 <!-- END DATA TABLE-->
