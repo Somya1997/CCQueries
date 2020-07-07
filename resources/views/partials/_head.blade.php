ab8eb65b2934b066acd847f7f74f7ebb1ef86c2c
SidMnnit <sidd.mnnit@gmail.com>
2020-07-07 16:49:08 +0530

    http to https

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -2,7 +2,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,700|Oswald:400,700" rel="stylesheet">
- 
+
+   
     <link rel="stylesheet" href="{{ URL::asset('fonts/icomoon/style.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/jquery.fancybox.min.css') }}">
@@ -12,11 +13,10 @@
     <link rel="stylesheet" href="{{ URL::asset('css/aos.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/show.track.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('css/parsley.css') }}">
-    <link href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css" rel="Stylesheet" type="text/css" />
+    <link href="https://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css" rel="Stylesheet" type="text/css" />
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
     <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
     <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-
     <!-- MAIN CSS -->
     <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" type="text/css">
     <link href="{{asset('css/theme.css') }}" rel="stylesheet" media="all">
