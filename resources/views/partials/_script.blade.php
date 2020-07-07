edfa4b5ea94186cebcde76540d173f1756189aac
Somya1997 <43964018+Somya1997@users.noreply.github.com>
2020-07-07 16:25:39 +0530

    images working

--- resources/views/partials/_script.blade.php
+++ resources/views/partials/_script.blade.php
@@ -9,10 +9,6 @@
     <script src="<?php echo asset('js/jquery.easing.1.3.js')?>"></script>
     <script src="<?php echo asset('js/aos.js')?>"></script>
     <script type="text/javascript" src="{{ URL::asset('js/jquery.js') }}"></script>
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
-    <script type="text/javascript" src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
     <script src="http://parsleyjs.org/dist/parsley.js"></script>
     <script src="jquery.js"></script>
     <script src="parsley.min.js"></script>
